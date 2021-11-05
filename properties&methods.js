a2ef410b7e05baed1e97c7267b05270818e883b2
TuqaRidha123 <87614063+TuqaRidha123@users.noreply.github.com>
2021-11-05 15:27:35 -0400

    Add files via upload

---
+++ properties&methods.js
@@ -0,0 +1,22 @@
+// the important ones
+/*
+classList
+getAttribute()
+setAttribute()
+appendChild()
+append()
+prepend()
+removeChild()
+remove()
+createElement
+
+innerText
+textContent
+innerHTML
+value
+parentElement
+children
+nextSibling
+previousSibling
+style
+*/
